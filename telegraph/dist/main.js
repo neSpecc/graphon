917f726aee8085d2efdee5ebdd9934eb315edea7
Peter Savchenko <specc.dev@gmail.com>
2019-07-09 18:17:05 +0300

    replace icosn

--- telegraph/dist/main.js
+++ telegraph/dist/main.js
@@ -3440,13 +3440,13 @@ class header_Header {
               </svg>`
       },
       {
-        type: 'bar',
+        type: 'area',
         icon: `<svg width="21" height="18" xmlns="http://www.w3.org/2000/svg">
                 <path d="M21 11.133L8.445 12.926 5.27 8.694 0 11.705v-4.17l4.98-3.32 6.838 4.884L21 6.344v4.789zm0 2.02V16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-1.991l4.73-2.703 2.825 3.768L21 13.153zm0-8.897l-8.818 2.645L5.02 1.785 0 5.131V2a2 2 0 0 1 2-2h17a2 2 0 0 1 2 2v2.256z" fill="#979797" fill-rule="evenodd"/>
               </svg>`
       },
       {
-        type: 'area',
+        type: 'bar',
         icon: `<svg width="19" height="18" xmlns="http://www.w3.org/2000/svg">
                 <g fill="#979797" fill-rule="evenodd">
                   <rect y="10" width="3" height="8" rx="1.5"/>
@@ -3454,8 +3454,8 @@ class header_Header {
                   <rect x="16" y="9" width="3" height="9" rx="1.5"/>
                   <path d="M7 10v4H4v-4h3zm0-1H4V6.5a1.5 1.5 0 0 1 3 0V9zm0 6v1.5a1.5 1.5 0 0 1-3 0V15h3zM11 14H8V7h3v7zm0 1v1.5a1.5 1.5 0 0 1-3 0V15h3zm0-9H8V1.5a1.5 1.5 0 0 1 3 0V6z"/>
                 </g>
-              </svg>`
-      }
+              </svg>`,
+      },
     ].forEach(({type, icon}) => {
       const switcher = make('span', header_Header.CSS.typeSwitcher);
 
@@ -3604,4 +3604,3 @@ class telegraph_Telegraph {
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=main.js.map
